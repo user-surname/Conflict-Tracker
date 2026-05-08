3459323bf60cff9fd7e9867e5bf6ed3ad1cc9a54
user-surname <smetannikovviacheslav@elpuig.xeill.net>
2026-05-08 18:24:54 +0200

    commit

---
+++ src/main/java/com/example/Conflict/Tracker/controller/config/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.example.Conflict.Tracker.controller.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "*"
+                )
+                .allowedMethods("*");
+    }
+
+}
--- src/main/java/com/example/Conflict/Tracker/controller/rest/ConflictResource.java
+++ src/main/java/com/example/Conflict/Tracker/controller/rest/ConflictResource.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping(ConflictResource.CONFLICTS)
 public class ConflictResource {
